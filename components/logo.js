74e40b090411e7cbb8cc1ffdf44fd3dd01a40dbf
robel.michael <robel.michael@onelab.se>
2022-11-05 19:32:23 +0100

    frontend animation startpage

--- components/logo.js
+++ components/logo.js
@@ -1,13 +1,44 @@
 import React from "react";
 import { useRive } from "@rive-app/react-canvas";
 import RiveComponent from "@rive-app/react-canvas";
+import PharaoRiv from "./rive/pharao.riv";
+import { AnimatedTitle } from "./animation/AnimatedText/AnimationText";
 
 export const Logo = () => {
+  // const { rive } = useRive({
+  //   src: "../pharao.riv",
+  //   autoplay: false,
+  // });
   return (
     <div class="flex h-screen justify-center">
       <div class="mt-10 flex h-5/6 w-4/6 rounded-md ">
         <div class="space-y-30 flex w-full flex-col items-center">
-          <RiveComponent src="./rive/pharao.riv" />
+          <div class="body-font mt-10 font-poppins text-7xl font-bold ">
+            <AnimatedTitle
+              size={3}
+              weight={600}
+              time={1}
+              text="WELCOME TO NFT MARKET"
+            />
+          </div>
+          <div class="body-font mt-10 font-poppins text-2xl">
+            <AnimatedTitle
+              size={2}
+              weight={300}
+              time={4}
+              text="THE BEST NFT MARKET IN THE PLANET"
+            />
+          </div>
+          <div class="body-font mt-10 font-poppins text-2xl">
+            <AnimatedTitle
+              size={1}
+              weight={150}
+              time={8}
+              text="START BUY CREATING YOUR FIRST NFT"
+            />
+          </div>
+
+          {/* <RiveComponent src={PharaoRiv} className="base-canvas-size" /> */}
         </div>
       </div>
     </div>
