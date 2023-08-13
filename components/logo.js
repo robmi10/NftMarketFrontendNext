4a5c5834692010fb0efeb507feec346939389d26
robmi10 <89600540+robmi10@users.noreply.github.com>
2023-08-13 14:58:53 +0200

    wip

--- components/logo.js
+++ components/logo.js
@@ -13,12 +13,13 @@ export const Logo = () => {
     <div className="flex h-screen justify-center">
       <div className="mt-10 flex h-5/6 w-4/6 rounded-md ">
         <div className="space-y-30 flex w-full flex-col items-center">
-          <div className="body-font mt-11 font-poppins text-6xl font-bold ">
-            NFT MARKET
+          <div className="body-font mt-10 font-poppins text-6xl font-bold ">
+            WELCOME TO THE
           </div>
-          <div className="body-font mt-10 font-poppins text-3xl">
-            THE BEST NFT MARKET IN THE WORLD
+          <div className="body-font mt-10 font-poppins text-4xl font-bold ">
+            BEST NFT MARKET IN THE WORLD
           </div>
+
           <div className="body-font mt-10 font-poppins text-2xl">
             START BUY CREATING YOUR FIRST NFT
           </div>
